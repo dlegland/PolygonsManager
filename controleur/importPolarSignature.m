a2b46013d5612f8099bcb930f2dab81bcc109ae9
tcorcel <corcel.thomas@gmail.com>
2016-01-20 16:00:55 +0100

    Added polar signature importing

---
+++ controleur/importPolarSignature.m
@@ -0,0 +1,17 @@
+function importPolarSignature(~,~, obj)
+[fName, fPath] = uigetfile('C:\Stage2016_Thomas\data_plos\slabs\images\*.txt');
+fFile = fullfile(fPath, fName);
+
+if ~isempty(obj.handles.panels)
+    obj = MainFrame;
+end
+
+import = Table.read(fFile);
+
+pas = 360/columnNumber(import);
+startAngle = str2double(import.colNames{1});
+angles = startAngle:pas:360+startAngle-pas;
+polygons = PolarSignatureArray(import.data, angles);
+
+setPolygonArray(obj, import.rowNames, polygons)
+end

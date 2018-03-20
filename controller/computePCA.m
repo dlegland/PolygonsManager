c486666880982e03ea75a3a7a4d1d3341e5a43e5
dlegland <david.legland@inra.fr>
2018-03-20 16:28:04 +0100

    update PCA computation

--- controller/computePCA.m
+++ controller/computePCA.m
@@ -1,11 +1,23 @@
-function computePCA(obj)
+function computePCA(frame)
 %COMPUTEPCA  Compute the Principal Component Analysis of the current polygons
 %
 %   Inputs :
 %       - obj : handle of the MainFrame
 %   Outputs : none
 
-obj.model.pca = Pca(Table.create(getDatas(obj.model.PolygonArray), 'rowNames', obj.model.nameList'), 'Display', 'none', 'scale', false);
-
-updateMenus(obj);
+% extract data from polygon array, depending on its class
+array = frame.model.PolygonArray;
+if isa(array, 'CoordsPolygonArray')
+    data = array.coords;
+elseif isa(array, 'PolarSignatureArray')
+    data = array.signatures;
+else
+    error(['Unable to compute data table from PolygonArray with class: ' class(array)]);
 end
+
+% transform into data table, and compute PCA
+tab = Table.create(data, 'rowNames', frame.model.nameList');
+frame.model.pca = Pca(tab, 'Display', 'none', 'scale', false);
+
+% refresh menus
+updateMenus(frame);
